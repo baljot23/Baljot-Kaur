e8382795f3d6aa980f20f7f212b0378c1232bdf0
baljot23 <92813636+baljot23@users.noreply.github.com>
2022-08-03 20:34:48 -0400

    removed build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Brands from "./components/Brands";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Portfolio from "./components/Portfolio";
@@ -18,7 +17,6 @@ const App = () => {
         <Header />
         <Hero />
         <Skills />
-        {/* <Brands /> */}
         <About />
         <Portfolio />
         <Services />
